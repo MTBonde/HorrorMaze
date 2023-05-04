dd877a9677b41057451507e418c9ef657841476c
NillerA <113096753+NillerA@users.noreply.github.com>
2023-05-04 10:02:43 +0200

    minor fix

--- HorrorMaze/Scenes/GameScreen.cs
+++ HorrorMaze/Scenes/GameScreen.cs
@@ -18,7 +18,7 @@ namespace HorrorMaze
         {
             camTarget = new Vector3(5f, 1f, 0.75f);//look target will be replaced by PlayerController and Camera component
             camPosition = new Vector3(5f, -0f, 50f);//the players start location will be replaced by the Camera component later
-            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GameWorld.Instance.graphics.GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);//sets up the projection matrix to a field of view on 45 degrees
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GameWorld.Instance.Graphics.GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);//sets up the projection matrix to a field of view on 45 degrees
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, new Vector3(0f, 0f, 1f));//Sets Z as the upwards axis for the view
             worldMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Up, Vector3.Backward);//Sets Z as the upwards axis for the World and y as the forward
 
