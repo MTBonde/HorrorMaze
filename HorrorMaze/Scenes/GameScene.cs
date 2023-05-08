e217f3e1de2ae7a0a4ac757b833e00ec0d5c3f3c
NillerA <113096753+NillerA@users.noreply.github.com>
2023-05-08 23:27:12 +0200

    deleted a few lines

--- HorrorMaze/Scenes/GameScene.cs
+++ HorrorMaze/Scenes/GameScene.cs
@@ -18,10 +18,7 @@ namespace HorrorMaze
         public override void SetupScene()
         {
             //GameWorld.Instance.IsMouseVisible = true;
-            //camTarget = new Vector3(0f, 0f, 0f);//look target will be replaced by PlayerController and Camera component
-            //camPosition = new Vector3(0f, -5f, 0);//the players start location will be replaced by the Camera component later
-            //projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GameWorld.Instance.Graphics.GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);//sets up the projection matrix to a field of view on 45 degrees
-            //viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, new Vector3(0f, 0f, 1f));//Sets Z as the upwards axis for the view
+            //creates worlds center point
             worldMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Forward, Vector3.Up);
 
             //Test maze
