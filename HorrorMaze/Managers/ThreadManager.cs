2c94aa51b1067188fad4b903a563eca6fb0b3094
Sky1pigeon <112692970+Sky1pigeon@users.noreply.github.com>
2023-05-09 10:04:56 +0200

    added killing to threads

--- HorrorMaze/Managers/ThreadManager.cs
+++ HorrorMaze/Managers/ThreadManager.cs
@@ -13,19 +13,51 @@ namespace HorrorMaze
     {
         static bool kill_thread = false;
         static List<GameObject> enemies = new List<GameObject>();
+        static List<bool> threads_lifeline = new List<bool>();
         public static void Startup(GameObject enemy)
         {
             enemies.Add(enemy);
+            bool pathings = false;
+            threads_lifeline.Add(pathings);
             Thread pathing = new Thread(UpdatePathing);
             pathing.IsBackground = true;
             pathing.Start();
         }
         static void UpdatePathing()
         {
-            while (!kill_thread)
+            while (!threads_lifeline[0])
             {
                 enemies[0].Start();
                 Thread.Sleep(10 * 1000);
+            }
+        }
+        static void KillÁllThreads()
+        {
+            for (int i = 0; i < threads_lifeline.Count; i++)
+            {
+                threads_lifeline[i] = true;
+            }
+            Thread killing = new Thread(KillThreading);
+            killing.IsBackground = true;
+            killing.Start();
+        }
+        static void KillThreading()
+        {
+            bool done = false;
+            bool check_done = true;
+            while (!done)
+            {
+                check_done = true;
+                for (int i = 0; i < threads_lifeline.Count; i++)
+                {
+                    if (threads_lifeline[i] == true)
+                    {
+                        threads_lifeline.RemoveAt(i);
+                        check_done = false;
+                    }
+                }
+                if (check_done)
+                    done = true;
             }
         }
     }
