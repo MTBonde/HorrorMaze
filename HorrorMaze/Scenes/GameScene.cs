40e2d7622e52a688781f0b74234f2583dc13a207
MTBonde <Thorbonde@gmail.com>
2023-05-10 09:33:21 +0200

    fixed gamobject naming in gamescene

--- HorrorMaze/Scenes/GameScene.cs
+++ HorrorMaze/Scenes/GameScene.cs
@@ -30,15 +30,10 @@ namespace HorrorMaze
             go.AddComponent<MazeCollider>().SetMaze(cells);
 
             //test enemy
-            go = new GameObject();
-            go.name = "Enemy";
-            go.AddComponent<BoxCollider>().size = new Vector3(1, 1, 1);
-            go.AddComponent<MeshRenderer>().SetModel("ghost_rig");
-            go.transform.Position3D = new Vector3(0.5f, 0.5f, 0);
-            go.AddComponent<Pathing>().mazeCells = cells;
-            go.AddComponent<Enemy>();
+           
             GameObject enemy = new GameObject();
-            enemy = new GameObject();
+            enemy.name = "Enemy";
+            enemy.AddComponent<BoxCollider>().size = new Vector3(1, 1, 1);            
             enemy.AddComponent<MeshRenderer>().SetModel("ghost_rig");
             enemy.transform.Position3D = new Vector3(0.5f, 0.5f, 0);
             enemy.AddComponent<Pathing>().mazeCells = cells;
@@ -52,33 +47,22 @@ namespace HorrorMaze
             audioManager.AddAudioSource(enemyAudioSource);
 
             //test player
-            GameObject player = new GameObject();           
-            player.transform.Position3D = new Vector3(0.5f, 1.5f, 1.6f);
+            GameObject player = new GameObject();
+            player.transform.Position3D = new Vector3(1.5f, 1.5f, 1.6f);            
             player.transform.Rotation = new Vector3(0, 0, 0);
             player.AddComponent<PlayerController>();
             player.AddComponent<Camera>();
             // Set up the listener AudioComponent and attach it to the player
             PlayerAudioListener playerAudioListener = player.AddComponent<PlayerAudioListener>();
 
-
-            //test cam
-            go = new GameObject();
-            go.transform.Position3D = new Vector3(1.5f, 1.5f, 1.6f);
-            go.transform.Rotation = new Vector3(0, 0, 0);
-            go.AddComponent<PlayerController>();
-            go.AddComponent<Camera>();
-
             //test Goal
-            go = new GameObject();
-            go.name = "Goal";
-            go.transform.Position3D = new Vector3(4.5f,4.5f,0);
-            go.AddComponent<MeshRenderer>().SetModel("win_item");
-            go.AddComponent<BoxCollider>().size = Vector3.One / 10;
+            GameObject goal = new GameObject();
+            goal.name = "Goal";
+            goal.transform.Position3D = new Vector3(4.5f,4.5f,0);
+            goal.AddComponent<MeshRenderer>().SetModel("win_item");
+            goal.AddComponent<BoxCollider>().size = Vector3.One / 10;
+         
 
-            //test UI
-            go = new GameObject();
-            go.AddComponent<SpriteRenderer>().SetSprite("Company Logo");
-            go.transform.Position = new Vector2(200,200);
             // Add the EnemyAudioController to the enemy object and set its properties:
             EnemyAudioController enemyAudioController = enemy.AddComponent<EnemyAudioController>();
             enemyAudioController.Setup(enemyAudioSource, playerAudioListener, audioManager);
