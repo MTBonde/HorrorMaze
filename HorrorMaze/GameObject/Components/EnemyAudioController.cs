de35dcb1c5ef09590f6204b35a255412bb2eb0e0
MTBonde <Thorbonde@gmail.com>
2023-05-16 20:22:07 +0200

    Revert "added more growl sounds and ambience stingers"
    This reverts commit f8303202e2a49d0a654abba79721b1f5f61200dc.

--- HorrorMaze/GameObject/Components/EnemyAudioController.cs
+++ HorrorMaze/GameObject/Components/EnemyAudioController.cs
@@ -66,7 +66,7 @@ namespace HorrorMaze
                 }
                 else
                 {
-                    //CalculateVolumenBasedOnDistance("heartbeat", distance, maxDistanceHeartbeat);
+                    CalculateVolumenBasedOnDistance("heartbeat", distance, maxDistanceHeartbeat);
                 }
             }
             else if(_enemyAudioSource._SoundEffectsPlaying.ContainsKey("heartbeat"))
