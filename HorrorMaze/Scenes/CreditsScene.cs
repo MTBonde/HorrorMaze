b9c2bbbaf093a87030b026469fe5ab7b69ae9c4c
MTBonde <Thorbonde@gmail.com>
2023-05-26 11:56:13 +0200

    fix to credit screen

--- HorrorMaze/Scenes/CreditsScene.cs
+++ HorrorMaze/Scenes/CreditsScene.cs
@@ -12,16 +12,15 @@ namespace HorrorMaze
     /// </summary>
     public class CreditsScene : Scene
     {
-
-
-
         public override void SetupScene()
         {
+            var screenHeightSpace = GameWorld.Instance.GraphicsDevice.Viewport.Height / 10;
+            
             GameObject CreditTextLine1Go = new GameObject();
             TextRenderer CreditTextLine1 = CreditTextLine1Go.AddComponent<TextRenderer>();
             CreditTextLine1.scale = 20;
             CreditTextLine1.color = Color.Red;
-            CreditTextLine1.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 100);
+            CreditTextLine1.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace);
             CreditTextLine1.SetText("Horror Maze");
 
 
@@ -29,7 +28,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine2 = CreditTextLine2Go.AddComponent<TextRenderer>();
             CreditTextLine2.scale = 5;
             CreditTextLine2.color = Color.Red;
-            CreditTextLine2.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 300);
+            CreditTextLine2.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*2);
             CreditTextLine2.SetText("was made by");
 
 
@@ -37,7 +36,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine3 = CreditTextLine3Go.AddComponent<TextRenderer>();
             CreditTextLine3.scale = 10;
             CreditTextLine3.color = Color.Red;
-            CreditTextLine3.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 400);
+            CreditTextLine3.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*2.5f);
             CreditTextLine3.SetText("ScareFactory");
 
 
@@ -45,7 +44,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine4 = CreditTextLine4Go.AddComponent<TextRenderer>();
             CreditTextLine4.scale = 5;
             CreditTextLine4.color = Color.Red;
-            CreditTextLine4.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 700);
+            CreditTextLine4.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*5);
             CreditTextLine4.SetText("M.T.Bonde, N.N.Andersen & T.S.Dietrich");
 
 
@@ -53,7 +52,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine5 = CreditTextLine5Go.AddComponent<TextRenderer>();
             CreditTextLine5.scale = 5;
             CreditTextLine5.color = Color.Red;
-            CreditTextLine5.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 800);
+            CreditTextLine5.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*6);
             CreditTextLine5.SetText("3d-Models by N.N.Andersen");
 
 
@@ -61,7 +60,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine6 = CreditTextLine6Go.AddComponent<TextRenderer>();
             CreditTextLine6.scale = 5;
             CreditTextLine6.color = Color.Red;
-            CreditTextLine6.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 900);
+            CreditTextLine6.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*7);
             CreditTextLine6.SetText("Music by Lofi-Lou. Subscribe on youtube");
 
 
@@ -69,7 +68,7 @@ namespace HorrorMaze
             TextRenderer CreditTextLine7 = CreditTextLine7Go.AddComponent<TextRenderer>();
             CreditTextLine7.scale = 5;
             CreditTextLine7.color = Color.Red;
-            CreditTextLine7.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, Globals.ScreenSize.Y -100);
+            CreditTextLine7.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace *9);
             CreditTextLine7.SetText("Horror Maze was made using the Monogame Framework");
             CreditTextLine7Go.AddComponent<MainMenuTimer>();
 
