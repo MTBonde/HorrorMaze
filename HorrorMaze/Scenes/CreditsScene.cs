806210bafa7b77d2128aa7d298bbe79833876ccf
MTBonde <Thorbonde@gmail.com>
2023-05-29 15:10:14 +0200

    credits comment by thor

--- HorrorMaze/Scenes/CreditsScene.cs
+++ HorrorMaze/Scenes/CreditsScene.cs
@@ -1,17 +1,22 @@
 ﻿namespace HorrorMaze
 {
     /// <summary>
-    /// CreditScene
+    /// This class creates the credits 
     /// Thor
     /// </summary>
     public class CreditsScene : Scene
     {
+        /// <summary>
+        /// The SetupScene method is responsible for setting up Credit text.
+        /// </summary>
         public override void SetupScene()
         {
-
+            // Calculate the space each line of text should take on screen based on screen height and a divider factor (10)
             float screenHeightSpace = GameWorld.Instance.GraphicsDevice.Viewport.Height / 10f;
+            // Calculate a screen resolution factor based on current screen height compared to a base resolution (1080p)
             float ScreenResolutionFactor = GameWorld.Instance.GraphicsDevice.Viewport.Height / 1080f;
 
+            // Call Createtext for each line of text to display, set "text", textsize and position as argument
             CreateText("Horror Maze", 20 * ScreenResolutionFactor, new Vector2(Globals.ScreenCenterWidth.X, 100 * ScreenResolutionFactor));
             CreateText("was made by", 5 * ScreenResolutionFactor, new Vector2(Globals.ScreenCenterWidth.X, 100 * ScreenResolutionFactor * 2.25f));
             CreateText("ScareFactory", 10 * ScreenResolutionFactor, new Vector2(Globals.ScreenCenterWidth.X, 100 * ScreenResolutionFactor * 3));
@@ -21,84 +26,34 @@
             CreateText("Horror Maze was made using the Monogame Framework", 5 * ScreenResolutionFactor, new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace * 9));
         }
 
+        /// <summary>
+        /// The CreateText method is used to create a new text object in the scene.
+        /// It sets the text string, size, color, and position.
+        /// If the text is about the Monogame Framework, it also adds a MainMenuTimer component.
+        /// </summary>
+        /// <param name="text">The text string to be displayed.</param>
+        /// <param name="textsize">The size of the text to be displayed.</param>
+        /// <param name="position">The position of the text in the scene.</param>
         private void CreateText(string text, float textsize, Vector2 position)
         {
-            GameObject creditTextGo = new GameObject();
-            TextRenderer creditText = creditTextGo.AddComponent<TextRenderer>();
+            // Create a new game object for the text
+            GameObject creditTextGO = new GameObject();
+            // Add a TextRenderer component to the game object
+            TextRenderer creditText = creditTextGO.AddComponent<TextRenderer>();
+            // Set the scale of the text using the text size parametre
             creditText.scale = textsize;
+            // Set the color of the text to red
             creditText.color = Color.Red;
+            // Position the text in the scene
             creditText.transform.Position = position;
+            // Set the text content
             creditText.SetText(text);
 
+            // Add a timer to the last text, so that player is sent back to title screen after a time
             if(text == "Horror Maze was made using the Monogame Framework")
             {
-                creditTextGo.AddComponent<MainMenuTimer>();
+                creditTextGO.AddComponent<MainMenuTimer>();
             }
         }
     }
-    //      GameObject CreditTextLine1Go = new GameObject();
-    //    TextRenderer CreditTextLine1 = CreditTextLine1Go.AddComponent<TextRenderer>();
-    //    CreditTextLine1.textsize = 20 * ScreenResolutionFactor;
-    //    CreditTextLine1.color = Color.Red;
-    //    CreditTextLine1.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, 100 * ScreenResolutionFactor);
-    //    CreditTextLine1.SetText("Horror Maze");
-
-
-    //    GameObject CreditTextLine2Go = new GameObject();
-    //    TextRenderer CreditTextLine2 = CreditTextLine2Go.AddComponent<TextRenderer>();
-    //    CreditTextLine2.textsize = 5 * ScreenResolutionFactor;
-    //    CreditTextLine2.color = Color.Red;
-    //    CreditTextLine2.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, CreditTextLine1.transform.Position.Y * 2.25f);
-    //    CreditTextLine2.SetText("was made by");
-
-
-    //    GameObject CreditTextLine3Go = new GameObject();
-    //    TextRenderer CreditTextLine3 = CreditTextLine3Go.AddComponent<TextRenderer>();
-    //    CreditTextLine3.textsize = 10 * ScreenResolutionFactor;
-    //    CreditTextLine3.color = Color.Red;
-    //    CreditTextLine3.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, CreditTextLine1.transform.Position.Y * 3);
-    //    CreditTextLine3.SetText("ScareFactory");
-
-
-    //    GameObject CreditTextLine4Go = new GameObject();
-    //    TextRenderer CreditTextLine4 = CreditTextLine4Go.AddComponent<TextRenderer>();
-    //    CreditTextLine4.textsize = 5 * ScreenResolutionFactor;
-    //    CreditTextLine4.color = Color.Red;
-    //    CreditTextLine4.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace*5);
-    //    CreditTextLine4.SetText("M.T.Bonde, N.N.Andersen & T.S.Dietrich");
-
-
-    //    GameObject CreditTextLine5Go = new GameObject();
-    //    TextRenderer CreditTextLine5 = CreditTextLine5Go.AddComponent<TextRenderer>();
-    //    CreditTextLine5.textsize = 5 * ScreenResolutionFactor;
-    //    CreditTextLine5.color = Color.Red;
-    //    CreditTextLine5.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, CreditTextLine4.transform.Position.Y * 1.1f);
-    //    CreditTextLine5.SetText("3d-Models by N.N.Andersen");
-
-
-    //    GameObject CreditTextLine6Go = new GameObject();
-    //    TextRenderer CreditTextLine6 = CreditTextLine6Go.AddComponent<TextRenderer>();
-    //    CreditTextLine6.textsize = 5 * ScreenResolutionFactor;
-    //    CreditTextLine6.color = Color.Red;
-    //    CreditTextLine6.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, CreditTextLine4.transform.Position.Y * 1.2f);
-    //    CreditTextLine6.SetText("Music by Lofi-Lou. Subscribe on youtube");
-
-
-    //    GameObject CreditTextLine7Go = new GameObject();
-    //    TextRenderer CreditTextLine7 = CreditTextLine7Go.AddComponent<TextRenderer>();
-    //    CreditTextLine7.textsize = 5 * ScreenResolutionFactor;
-    //    CreditTextLine7.color = ;
-    //    CreditTextLine7.transform.Position = new Vector2(Globals.ScreenCenterWidth.X, screenHeightSpace *9);
-    //    CreditTextLine7.SetText("Horror Maze was made using the Monogame Framework");
-    //    CreditTextLine7Go.AddComponent<MainMenuTimer>();
-
-
-
-
-
-
-
-
-
-
 }
